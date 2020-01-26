6d1201fe5cdf954d7942354b34d3d379fc721dc3
Lenka2023 <lenka20231@gmail.com>
2020-01-26 19:31:41 +0200

    made AddForm

--- myblog/src/App.js
+++ myblog/src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import {HeaderBlock, PostItem } from 'components';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import {HeaderBlock, PostItem, AddForm } from 'components';
 class App extends Component{
   render(){
     return(
@@ -11,34 +13,14 @@ class App extends Component{
        />
         <div className="container">
               <div className="content">
-                 <button type="button" className="btn btn-primary">
+                {/* <button type="button" className="btn btn-primary">
               Add post
-                  </button>
+                  </button>*/}
                   <div className="content">
-                     
-                               <form className="add-form">
-  <div className="form-group">
-   <div className="add-form__row">
-          <h4>
-            <label for="title">Title</label>
-          </h4>
-          <input type="text" className="form-control" id="title" placeholder="Enter email" />
-        </div>
-        <div className="add-form__row">
-          <h4>
-            <label for="image">Image URL</label>
-          </h4>
-          <input type="text" className="form-control" id="image" placeholder="Enter image URL" />
-        </div>
-        <div className="add-form__row">
-          <h4>
-            <label for="description">Description</label>
-          </h4>
-          <textarea rows="8" className="form-control" placeholder="Enter text" id="description" placeholder="Enter description"/>
-        </div>
-      </div>
-   <button type="submit" className="btn btn-primary">Submit</button>
-</form>    
+                    {/*<div className="post-items"> 
+                          <PostItem _id="1" title="Заголовок статьи" createdAt="Fri Nov 02 2018"/> 
+                                                                              </div>*/}     
+                               <AddForm/> 
                             </div>
                         </div>    
                     </div>
--- myblog/src/components/index.js
+++ myblog/src/components/index.js
@@ -1,3 +1,3 @@
-import HeaderBlock from './HeaderBlock';
-import PostItem from './PostItem';
-export {HeaderBlock, PostItem};
+export { default as HeaderBlock } from './HeaderBlock';
+export { default as PostItem } from './PostItem';
+export { default as AddForm } from './AddForm';
